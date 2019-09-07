e6d489be6c5fb19490536fae40c201eaf1e4bb0e
dosisod <39638017+dosisod@users.noreply.github.com>
2019-09-06 23:09:49 -0700

    finished up game

--- destroy.js
+++ destroy.js
@@ -1,5 +1,12 @@
-//where the player is at (from 0-99)
+//where the player is at (from 0-49)
 var at=0
+
+//current time elapsed
+var active=false
+var reset=false
+var timer_interval=null
+var timer_start=0
+var timer_now=0
 
 var tfs=[
 	"true",
@@ -7,8 +14,18 @@ var tfs=[
 	"\"\"",
 	"\"hello\"",
 	"0",
-	"1"
+	"1",
+	"[]"
 ]
+
+var tfs2=[
+	"([1,2,3].indexOf(5)>0)",
+	"([1,2,3].indexOf(2)>0)",
+	"(\"x\".startsWith(\"x\")==true)",
+	"(\"x\".startsWith(\"y\")==true)"
+]
+
+var both=[...tfs, ...tfs2]
 
 //operations that are able to be put at the start of a statement
 var starts=[
@@ -27,8 +44,7 @@ var ops=[
 ]
 
 function randarr(arr) { //given an arry, select a random element
-	tmp=arr[~~(Math.random()*arr.length)]
-	return tmp
+	return arr[~~(Math.random()*arr.length)]
 }
 
 function bool_(b) {
@@ -62,14 +78,14 @@ function string(str) { //turns given string into a string element
 function init() {
 	ans=new Array(100)
 
-	for (i=0;i<100;i++) {
+	for (i=0;i<50;i++) {
 		tmp=generate(i)
-		ans[99-i]=tmp[1]
+		ans[49-i]=tmp[1]
 
 		li=document.createElement("li")
 		li.classList.add("hide")
 		li.innerHTML=tmp[0]
-		li.id="_"+(99-i)
+		li.id="_"+(49-i)
 		document.getElementById("exprs").appendChild(li)
 	}
 	document.getElementById("_0").classList.remove("hide")
@@ -77,7 +93,7 @@ function init() {
 }
 
 function generate(lvl) {
-	lvl=99-lvl
+	lvl=49-lvl
 	raw="" //stores raw text to be sent to compiler
 	html=""
 	if (lvl==0) {
@@ -89,11 +105,11 @@ function generate(lvl) {
 		html+=auto(tmp)
 		raw+=tmp
 
-		tmp=randarr(tfs)
+		tmp=randarr(both)
 		html+=auto(tmp)
 		raw+=tmp
 	}
-	else if (10<lvl&&lvl<30) {
+	else if (10<lvl&&lvl<=30) {
 		tmp=randarr(starts)
 		html+=auto(tmp)
 		raw+=tmp
@@ -110,15 +126,32 @@ function generate(lvl) {
 		html+=auto(tmp)
 		raw+=tmp
 	}
-	else {
-		html+="TBD"
-		raw+="true"
+	else if (30<lvl&&lvl<=50) {
+		tmp=randarr(tfs)
+		html+=auto(tmp)
+		raw+=tmp
+		
+		tmp=randarr(ops)
+		html+=auto(tmp)
+		raw+=tmp
+
+		tmp=randarr(tfs)
+		html+=auto(tmp)
+		raw+=tmp
+
+		tmp=randarr(ops)
+		html+=auto(tmp)
+		raw+=tmp
+
+		tmp=randarr(tfs)
+		html+=auto(tmp)
+		raw+=tmp
 	}
 	return [ html, compile(raw) ]
 }
 
 function compile(str) {
-	tmp=new Function("return "+str)
+	tmp=new Function("return !!"+str)
 	return !!tmp()
 }
 
@@ -126,10 +159,32 @@ function check(e) {
 	//only continue if enter was pressed
 	if (e.key!="Enter") return
 
-	input=document.getElementById("input").value.toLowerCase()
-	if (input!="true"&&input!="false") gameover()
+	if (at>=49) {
+		win()
+		return
+	}
 
-	if ((""+ans[at])!=input) gameover()
+	active=true
+
+	if (at==0) document.getElementById("startup").remove()
+
+	input=document.getElementById("input").value.toLowerCase()
+	if (input!="true"&&input!="false") return
+
+	if ((""+ans[at])!=input) {
+		gameover()
+		return
+	}
+
+	//everything worked, remove and reset things
+	timer_start=Date.now()
+	clearInterval(timer_interval)
+
+	timer_interval=setInterval(function() {
+		//after 10 seconds, run gameover function
+		gameover()
+		return
+	}, 10000)
 
 	document.getElementById("_"+at).remove()
 	document.getElementById("_"+(at+1)).classList.remove("hide")
@@ -138,6 +193,43 @@ function check(e) {
 	at++
 }
 
+function win() {
+	active=false
+	clearInterval(timer_interval)
+
+	document.getElementById("cod").innerHTML=""
+
+	document.getElementById("exprs").innerHTML=""
+	document.getElementById("timer").style.width="0%"
+	document.body.style.background="white"
+
+	document.getElementById("score").innerText+="100%"
+	document.getElementById("msg").innerText="You Win!"
+
+	document.getElementById("gameover").classList.remove("gameover")
+}
+
+function gameover() {
+	active=false //stop timer
+	clearInterval(timer_interval)
+
+	document.getElementById("cod").innerText+=document.getElementById("_"+at).innerText
+
+	document.getElementById("exprs").innerHTML=""
+	document.getElementById("timer").style.width="0%"
+	document.body.style.background="white"
+
+	document.getElementById("score").innerText+=(at*2)+"%"
+	document.getElementById("gameover").classList.remove("gameover")
+}
+
 window.onload=function() {
 	init()
+
+	setInterval(function() {
+		if (active) {
+			percent=((Date.now()-timer_start)%10000)/100
+			document.getElementById("timer").style.width=percent+"%"
+		}
+	}, 100)
 }
